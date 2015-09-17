c7b5adca9c26b3a1387f35c340dba28772366a90
Tyler Onufer <tyler.onufer@asti-usa.com>
2015-09-17 11:16:05 -0400

    Add images, refactor to use bootstrap.

--- main.go
+++ main.go
@@ -57,6 +57,16 @@ func main() {
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// start the room for clients to connect to
